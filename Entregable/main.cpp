db8240a488a86ce3d67e4106c3e4fc1b9e4a32d3
Hiromi00 <55908313+hiromi00@users.noreply.github.com>
2020-03-18 16:33:03 -0600

    Create main.cpp

---
+++ Entregable/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "mainMenu.h"
+using namespace std;
+
+
+int main(){
+    /*El programa se inicia a traves de un objeto creado desde MainMenu*/
+    MainMenu main;
+    main.menu();
+
+    return 0;
+}
